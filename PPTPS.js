9897593a4bc961c831a2f08fe3e3d22c4d6491a1
nekher <max.mym@gmail.com>
2022-09-26 21:47:58 -0300

    fsdfs

--- PPTPS.js
+++ PPTPS.js
@@ -35,84 +35,116 @@ var papel = 2;
 var roca = 3;
 var lagarto = 4;
 var spock = 5;
-var empates = 0;
+var eleccion_nomenclada_persona = '';
+var eleccion_nomenclada_npc = '';
 
 function juguemos(){
-    alert('Bienvenido a Roca, Papel, Tijera, Lagarto, Spock. elegi lo que queres seleccionar y luego y apreta el boton');
-    iniciar_ronda();
-    definir_ronda();
-    ronda();
-}
-
-function iniciar_ronda(){
     eleccion_persona = document.querySelector('#feleccion').value;
-    eleccion_npc = Math.random()*(5-1)+1;
+    eleccion_npc = (Math.random()*(5-1)+1).toFixed();
+    nomenclar_persona();
+    nomenclar_npc();
+    definir_ronda();
+    
 }
-
-
+function nomenclar_persona(){
+    if (eleccion_persona == 1){
+        eleccion_nomenclada_persona = 'TIJERA';
+    } else if (eleccion_persona == 2){
+        eleccion_nomenclada_persona = 'PAPEL';
+    } else if (eleccion_persona == 3){
+        eleccion_nomenclada_persona = 'PIEDRA';
+    } else if (eleccion_persona == 4){
+        eleccion_nomenclada_persona = 'LAGARTO';
+    } else if (eleccion_persona == 5){
+        eleccion_nomenclada_persona = 'SPOCK';     
+    }   
+}
+function nomenclar_npc(){
+    if (eleccion_npc == 1){
+        eleccion_nomenclada_npc = 'TIJERA';
+    } else if (eleccion_npc == 2){
+        eleccion_nomenclada_npc = 'PAPEL';
+    } else if (eleccion_npc == 3){
+        eleccion_nomenclada_npc = 'PIEDRA';
+    } else if (eleccion_npc == 4){
+        eleccion_nomenclada_npc = 'LAGARTO';
+    } else if (eleccion_npc == 5){
+        eleccion_nomenclada_npc = 'SPOCK';   
+}
+}
 function definir_ronda(){
     if (eleccion_persona == eleccion_npc){
-        empates = (empates + 1);
-        alert('Alto empate, pa!, va de nuevo!!');
-        juguemos();
+        //window.location.replace("empate.html");
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio Lo mismo..Tremendo y anticlimatico EMPATE... continuemos, recorda, gana el mejor de 5');
+   
     } else if (eleccion_persona == 1 && eleccion_npc == 2){
         tot_persona = (tot_persona + 1);
-        ronda();
+       ronda();
+       prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 1 && eleccion_npc == 4){
         tot_persona = (tot_persona +1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 1) {
         tot_npc = (tot_npc + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Y te toco perder..., recorda, gana el mejor de 5');
     } else if (eleccion_persona == 2 && eleccion_npc == 3){
         tot_persona = (tot_persona + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 2 && eleccion_npc == 5){
         tot_persona = (tot_persona + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 2){
         tot_npc = (tot_npc + 1);
-        ronda();
+        ronda();       
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Y te toco perder..., recorda, gana el mejor de 5');
     } else if (eleccion_persona == 3 && eleccion_npc == 1){
         tot_persona = (tot_persona +1);
-        ronda();  
+        ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 3 && eleccion_npc == 4){
         tot_persona = (tot_persona + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 3){
         tot_npc = (tot_npc + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Y te toco perder..., recorda, gana el mejor de 5');
     } else if (eleccion_persona == 4 && eleccion_npc == 2){
         tot_persona = (tot_persona + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 4 && eleccion_npc == 5){
         tot_persona = (tot_persona + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 4){
         tot_npc = (tot_npc + 1);
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Y te toco perder..., recorda, gana el mejor de 5');
     } else if (eleccion_persona == 5 && eleccion_npc == 1){
         tot_persona = (tot_persona + 1 );
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona == 5 && eleccion_npc == 3){
         tot_persona = (tot_persona + 1 );
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Ganaste suertudo, continuemos, recorda, gana el mejor de 5');
     } else if (eleccion_persona = 5){
         tot_npc = (tot_npc + 1 );
         ronda();
+        prompt('Esta ronda, vos elegiste '+ eleccion_nomenclada_persona + ' y la PC eligio ' + eleccion_nomenclada_npc + ' Y te toco perder..., recorda, gana el mejor de 5');
     }
 }
 
 function ronda(){
-    if (tot_persona = 3){
-        alert ('GANASTE!!!, sos mejor que la PC, que CRA!!!');
-        alert ('Vas de nuevo?');
-        juguemos();
+      if (tot_persona == 3){
+        window.location.replace("ganaste.html");
+    } else if (tot_npc == 3){
+         window.location.replace("perdiste.html");
     }
-    if (tot_npc = 3){
-        alert ('PERDISTE!!!!, yo que vos me retiro del gaming.');
-        alert ('Personalmente apagaria la PC, pero...si queres vamos de nuevo');
-        juguemos();
-    }
-    juguemos();
+    
 }
